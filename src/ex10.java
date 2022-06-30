9cf7fb129d25ed28ea181c3b6e179bf7f8b5d4d6
André Jonas <andrejonaspesceromanoli@gmail.com>
2022-06-30 11:56:27 -0300

    ...

--- src/ex10.java
+++ src/ex10.java
@@ -4,12 +4,23 @@ public class ex10{
     public static void main(String[] args) throws Exception {
         try (Scanner leitor = new Scanner(System.in)) {
             int n=leitor.nextInt();
-            int i = 0;
+            //int y = 1;//
+            int i=1;
+            
             for (;i < n; i++);{
-            System.out.println(i + " " + (i*i) + " " + (i*i*i));
+                i=leitor.nextInt();
+                System.out.println(i+""+n);
+
         }
     }
             
-        }
+        } 
+        /*N = int(input())
+        Y = 1
+        
+        for N in range(N):
+         X = int(input())
+         print(f"resposta {Y}: {X}")
+         Y += 1 */
 
 }
---
+++ src/ex11.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ex11{
+    public static void main(String[] args) throws Exception {
+        try (Scanner leitor = new Scanner(System.in)) {
+            int i=1;
+            int j=60;
+            while (j >= 0) {
+                
+            System.out.println("iterador=" + i + " varJ=" + j);
+            j-=5;
+            i+=3;
+            }
+            }
+        }
+    }
+            
+        
+
+
